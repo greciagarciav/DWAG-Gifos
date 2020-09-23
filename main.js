7a4ea1e0ab4520bda6353ba66c490aa0ffc264bc
greciagarciav <garciav.grecia@gmail.com>
2020-09-23 18:20:43 -0500

    mobile home view advance

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import { getData } from './data.js';
 
-console.log(getData);
+let data = [];
+const inputGift = document.getElementById('topic-gift');
+const searchButton = document.getElementById('search-gift');
+
+searchButton.addEventListener('click', () => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=xRw1K9iEL7bkhCblwCyxd00ppSOBwLVE&q=${inputGift.value}`)
+        .then((resp) => resp.json())
+        .then((obj) => {
+            data = obj.data;
+        console.log('hola', inputGift.value, data);
+
+        });
+});
